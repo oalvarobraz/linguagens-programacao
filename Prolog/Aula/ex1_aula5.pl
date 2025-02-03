f4c869041eb3569d92d40149f87a63f4ab720c5e
oalvarobraz <alvarobraz2502@gmail.com>
2025-02-03 08:55:06 -0300

    feat: Adiciona funcionalidade de ler um arquivo recursivamente

--- Prolog/Aula/ex1_aula5.pl
+++ Prolog/Aula/ex1_aula5.pl
@@ -1,14 +1,29 @@
 principal :-
-    write('Entre como uma lista de números separados por vírgula: Ex.: [1,2,3,4,5].'), nl,
-    read(List),
-    sum_list(List, Sum),
+    ler_arquivo('input.txt', Numeros),
+    sum_list(Numeros, Sum),
     open('output.txt', write, F),
     write(F, Sum),
     close(F),
     write('Sum written to output.txt'), nl.
 
+ler_arquivo(Arquivo, Numeros) :-
+    open(Arquivo, read, File),
+    ler_linhas(File, Numeros),
+    close(File).
+
+ler_linhas(File, [Numero | Resto]) :-
+    read(File, Numero),
+    Numero \= end_of_file,
+    ler_linhas(File, Resto).
+ler_linhas(_, []).
 
 soma_lista([], 0).
 soma_lista([H|T], Soma) :-
     soma_lista(T, R),
     Soma is H + R.
+
+teste :-
+    open('input.txt', read, File),
+    read(File, Linha1),
+    read(File, Linha2),
+    close(File), write([Linha1, Linha2]),nl.
